afecce8f9b464bcaf258675e1e732ea317276dfb
L-Lemes <borgeslucaslemes@gmail.com>
2022-09-05 10:46:11 -0300

    finished project

---
+++ src/components/SubscribreButton/index.tsx
@@ -0,0 +1,46 @@
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+import styles from './style.module.scss'
+
+interface SubscribeButtonProps {
+  priceId: string
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(['pqp', err.message])
+    }
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
+}
+
+// href="https://checkout.stripe.com/pay/cs_test_a1iXH8LbjX1vs6ffAou259FE0cn4vbehDFtVfLm1Nn3wTyFctmps7N7gW7#fidkdWxOYHwnPyd1blpxYHZxWjA0T3JLQ0NOY3V1cn1KXENqZnA0cmttR0RuMzVqSWhdTDFHTFxPTGdtUU1WVDQ2dEgwNTF1aEE2cX9cZ1VUYmREU2ZKTUd%2FS0JncT12bT1mMklUU31PN3ZxNTVnTkpwSVR3SicpJ3VpbGtuQH11anZgYUxhJz8ncWB2cVo0MW5iNTcxbU4zVGFnN0BkQEAneCUl"
